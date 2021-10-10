08e3736ed62835515478b2e872dad7454a2ecae4
Micahel Sanabria <ssm1009631@est.univalle.edu>
2021-10-10 17:33:16 -0400

    actualizacion

--- Practica_SitioWebUnivalle/ScriptBDD/consultas1.sql
+++ Practica_SitioWebUnivalle/ScriptBDD/consultas1.sql
@@ -9,11 +9,14 @@ WHERE subjectID = 1
 
 SELECT * FROM users
 
-INSERT INTO users (name, lastName, userName, password, mail, type) values('Michael', 'Sanabria', 'ssm', HASHBYTES('md5','14900'), 'ssm1009631@est.univalle.edu', 1)
+INSERT INTO users (name, lastName, userName, password, mail, type) values('Michael', 'Sanabria', 'ssm', HASHBYTES('md5','asd'), 'ssm1009631@est.univalle.edu', 1)
 
 
-select mail from users
-where userName='ssm' and password=HASHBYTES('md5','14900')
+INSERT INTO users (name, lastName, userName, password, mail, type) values(@name, @lastName, @userName, HASHBYTES(@password,'14900'), @mail, @type)
+
+
+select * from users
+where password=0xD1C364F4B712DC6C804C2B773DE76D97
 
 select * from coments
 
